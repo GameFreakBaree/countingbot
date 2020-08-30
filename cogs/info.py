fe57281b6f8f45b0c4aa16fd413b40f856c47cd8
GameFreakBaree <33906209+GameFreakBaree@users.noreply.github.com>
2020-08-30 18:07:26 +0200

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -13,21 +13,6 @@ database = settings['database']
 
 read_settings.close()
 
-db_counting = mysql.connector.connect(
-    host=host,
-    database=database,
-    user=user,
-    passwd=passwd
-)
-
-counting_cursor = db_counting.cursor()
-counting_cursor.execute("SELECT footer FROM counting_settings")
-embed_footer = counting_cursor.fetchone()
-
-counting_cursor.execute("SELECT embed_color FROM counting_settings")
-embed_color_tuple = counting_cursor.fetchone()
-embed_color = int(embed_color_tuple[0], 16)
-
 
 class InfoCmd(commands.Cog):
 
@@ -36,6 +21,16 @@ class InfoCmd(commands.Cog):
 
     @commands.command()
     async def info(self, ctx):
+        db_counting = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
+
+        counting_cursor = db_counting.cursor()
+        counting_cursor.execute("SELECT footer FROM counting_settings")
+        embed_footer = counting_cursor.fetchone()
+
+        counting_cursor.execute("SELECT embed_color FROM counting_settings")
+        embed_color_tuple = counting_cursor.fetchone()
+        embed_color = int(embed_color_tuple[0], 16)
+
         embed = discord.Embed(
             description=f"__Created by:__ GameFreakBaree#9999\n__Shard:__ 1/1\n\n**Useful URLs**\n[Invite the Bot](https://discord.com/oauth2/authorize?client_id=742776969481158766&permissions=1074097233&redirect_uri=https%3A%2F%2Fdiscord.gg%2F5gvn5pn&scope=bot) - [Wiki](https://github.com/GameFreakBaree/countingbot/wiki) - [Support Server](https://discord.gg/5gvn5pn)",
             color=embed_color
@@ -43,6 +38,8 @@ class InfoCmd(commands.Cog):
         embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
         embed.set_footer(text=embed_footer[0])
         await ctx.send(embed=embed)
+
+        db_counting.close()
 
 
 def setup(client):
