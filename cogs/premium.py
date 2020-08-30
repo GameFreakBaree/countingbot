cc6d5555476bd0733045b98416c0edb782d31b12
GameFreakBaree <33906209+GameFreakBaree@users.noreply.github.com>
2020-08-30 18:08:05 +0200

    Update premium.py

--- cogs/premium.py
+++ cogs/premium.py
@@ -13,21 +13,6 @@ database = settings['database']
 
 read_settings.close()
 
-db_counting = mysql.connector.connect(
-    host=host,
-    database=database,
-    user=user,
-    passwd=passwd
-)
-
-counting_cursor = db_counting.cursor()
-counting_cursor.execute("SELECT footer FROM counting_settings")
-embed_footer = counting_cursor.fetchone()
-
-counting_cursor.execute("SELECT embed_color FROM counting_settings")
-embed_color_tuple = counting_cursor.fetchone()
-embed_color = int(embed_color_tuple[0], 16)
-
 
 class PremiumCmd(commands.Cog):
 
@@ -37,6 +22,16 @@ class PremiumCmd(commands.Cog):
     @commands.command()
     async def premium(self, ctx):
         if ctx.guild.id != 743513613943439422:
+            db_counting = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
+
+            counting_cursor = db_counting.cursor()
+            counting_cursor.execute("SELECT footer FROM counting_settings")
+            embed_footer = counting_cursor.fetchone()
+
+            counting_cursor.execute("SELECT embed_color FROM counting_settings")
+            embed_color_tuple = counting_cursor.fetchone()
+            embed_color = int(embed_color_tuple[0], 16)
+
             embed = discord.Embed(
                 description=f"Premium has not been activated on this server.\nPlease buy premium to support the Developer to make the project/bot and all future projects better.\n\n[Buy Premium](https://www.patreon.com/countingbot) - [Support Server](https://discord.gg/5gvn5pn)",
                 color=embed_color
@@ -44,6 +39,8 @@ class PremiumCmd(commands.Cog):
             embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
             embed.set_footer(text=embed_footer[0])
             await ctx.send(embed=embed)
+
+            db_counting.close()
 
 
 def setup(client):
