8be8f36594608d40cf551687f17d933d493e8f97
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-09-04 21:51:14 +0200

    Update premium.py

--- cogs/premium.py
+++ cogs/premium.py
@@ -1,17 +1,6 @@
 import discord
 from discord.ext import commands
-import mysql.connector
-import json
-
-with open('./db_settings.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-passwd = settings['passwd']
-database = settings['database']
-
-read_settings.close()
+from data import settings
 
 
 class PremiumCmd(commands.Cog):
@@ -21,26 +10,15 @@ class PremiumCmd(commands.Cog):
 
     @commands.command()
     async def premium(self, ctx):
-        if ctx.guild.id != 743513613943439422:
-            db_counting = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
-
-            counting_cursor = db_counting.cursor()
-            counting_cursor.execute("SELECT footer FROM counting_settings")
-            embed_footer = counting_cursor.fetchone()
-
-            counting_cursor.execute("SELECT embed_color FROM counting_settings")
-            embed_color_tuple = counting_cursor.fetchone()
-            embed_color = int(embed_color_tuple[0], 16)
-
-            embed = discord.Embed(
-                description=f"Premium has not been activated on this server.\nPlease buy premium to support the Developer to make the project/bot and all future projects better.\n\n[Buy Premium](https://www.patreon.com/countingbot) - [Support Server](https://discord.gg/5gvn5pn)",
-                color=embed_color
-            )
-            embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-            embed.set_footer(text=embed_footer[0])
-            await ctx.send(embed=embed)
-
-            db_counting.close()
+        embed = discord.Embed(
+            description=f"Premium has not been activated on this server."
+                        f"\nPlease buy premium to support the Developer to make the project/bot and all future projects better."
+                        f"\n\n[Buy Premium](https://www.patreon.com/countingbot) - [Support Server](https://discord.gg/5gvn5pn)",
+            color=settings.embedcolor
+        )
+        embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
+        embed.set_footer(text=settings.footer)
+        await ctx.send(embed=embed)
 
 
 def setup(client):
