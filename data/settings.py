e9b0bff291c0276e4615779b8eda94198e15873f
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-09-04 21:52:35 +0200

    Create settings.py

---
+++ data/settings.py
@@ -0,0 +1,10 @@
+footer = '© 2020 - CountingBot'
+embedcolor = 0x00B8BA
+succes_emote = '✅'
+error_emote = '❌'
+
+host = "DATABASE IP:PORT"
+user = "DATABASE USERNAME"
+database = "DATABASE NAME"
+passwd = "DATABASE PASSWORD"
+token = "YOUR TOKEN HERE"
