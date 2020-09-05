cf48de108b5a37ef57362e6d458616860831eb1d
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-09-05 21:00:36 +0200

    Update on_guild_join.py

--- cogs/on_guild_join.py
+++ cogs/on_guild_join.py
@@ -1,22 +1,12 @@
 from discord.ext import commands
 import mysql.connector
-import json
-
-with open('./db_settings.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-passwd = settings['passwd']
-database = settings['database']
-
-read_settings.close()
+from data import settings
 
 db_counting = mysql.connector.connect(
-    host=host,
-    database=database,
-    user=user,
-    passwd=passwd
+    host=settings.host,
+    user=settings.user,
+    passwd=settings.passwd,
+    database=settings.database
 )
 
 
@@ -30,18 +20,13 @@ class OnReady(commands.Cog):
         db_counting.commit()
         counting_cursor = db_counting.cursor()
 
-        instertguilddata = "INSERT INTO counting_guilddata (guild_id, totalcount) VALUES (%s, %s)"
-        record1 = (f"{guild.id}", 0)
-        counting_cursor.execute(instertguilddata, record1)
-        db_counting.commit()
-
         instertdata = "INSERT INTO counting_data (guild_id, number, last_user_id) VALUES (%s, %s, %s)"
         record2 = (f"{guild.id}", 0, 0)
         counting_cursor.execute(instertdata, record2)
         db_counting.commit()
 
-        instertguildsettings = "INSERT INTO counting_guildsettings (guild_id, prefix, counting_channel_id, maxcount, restart_on_error) VALUES (%s, %s, %s, %s, %s)"
-        record3 = (f"{guild.id}", "c!", 0, 2147483647, 0)
+        instertguildsettings = "INSERT INTO counting_guildsettings (guild_id, counting_channel_id, maxcount, restart_on_error) VALUES (%s, %s, %s, %s)"
+        record3 = (f"{guild.id}", 0, 2147483647, 0)
         counting_cursor.execute(instertguildsettings, record3)
         db_counting.commit()
 
