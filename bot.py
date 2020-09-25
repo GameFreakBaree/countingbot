08d591bfcb9118cfa5fd162180991be70e2ba28c
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-09-25 20:45:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,52 +4,24 @@ import discord
 from discord.ext import commands
 from data import settings
 
-client = commands.Bot(command_prefix='c.', case_insensitive=True)
+client = commands.Bot(command_prefix='c!', case_insensitive=True)
 client.remove_command("help")
 
 
 async def change_status():
     await client.wait_until_ready()
     while client.is_ready():
-        status = discord.Activity(name=f"c.help | {len(client.guilds)} Guilds", type=discord.ActivityType.watching)
+        status = discord.Activity(name=f"c!help | {len(client.guilds)} Servers", type=discord.ActivityType.watching)
         await client.change_presence(activity=status)
-        await asyncio.sleep(60)
+        await asyncio.sleep(30)
 
 
-@client.event
-async def on_ready():
-    print(f"[CountingBot] The bot is online and ready to use!")
-
-
-@client.command()
-async def load(ctx, extension):
-    if ctx.author.id == 643072638075273248:
-        client.load_extension(f'cogs.{extension}')
-        print(f"Load {extension}, by {ctx.author}")
-        await ctx.send(f"Load {extension}, succes!")
-
-
-@client.command()
-async def reload(ctx, extension):
-    if ctx.author.id == 643072638075273248:
-        client.unload_extension(f'cogs.{extension}')
-        client.load_extension(f'cogs.{extension}')
-        print(f"Reload {extension}, by {ctx.author}")
-        await ctx.send(f"Reload {extension}, succes!")
-
-
-@client.command()
-async def unload(ctx, extension):
-    if ctx.author.id == 643072638075273248:
-        client.unload_extension(f'cogs.{extension}')
-        print(f"Unload {extension}, by {ctx.author}")
-        await ctx.send(f"Unload {extension}, succes!")
-
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        print(f"[CountingBot] Cogs > {filename[:-3]} > Loaded!")
-        client.load_extension(f'cogs.{filename[:-3]}')
+for folder in settings.folder_list:
+    print(f"[{settings.bot_name}] ----------------------[ {folder.title()} ]--------------------")
+    for filename in os.listdir(f'./{folder}'):
+        if filename.endswith('.py'):
+            print(f"[{settings.bot_name}] {folder.title()} > {filename[:-3]} > Loaded!")
+            client.load_extension(f'{folder}.{filename[:-3]}')
 
 client.loop.create_task(change_status())
 client.run(settings.token)
